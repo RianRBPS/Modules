852752a2f7a6fc91528bd8099722fc99373f75f0
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-31 23:41:01 -0300

    Deleting files

--- 2.shutill_os.py
+++ 2.shutill_os.py
@@ -23,3 +23,15 @@ import shutil
 shutil.move(src='practice.txt',dst='C:\\Users\kingt\OneDrive\Desktop')
 
 os.listdir('C:\\Users\kingt\OneDrive\Desktop')
+
+##################################################
+
+'''Deleting Files
+NOTE: The os module provides 3 methods for deleting files:
+
+    os.unlink(path) which deletes a file at the path your provide
+    os.rmdir(path) which deletes a folder (folder must be empty) at the path your provide
+    shutil.rmtree(path) this is the most dangerous, as it will remove all files and folders contained in the path.
+All of these methods can not be reversed! Which means if you make a mistake you won't be able to recover the file. 
+Instead we will use the send2trash module.
+A safer alternative that sends deleted files to the trash bin instead of permanent removal.*'''
