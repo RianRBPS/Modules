b1e963c7456da4f0db4c2474aa474cfc25311158
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-31 23:54:40 -0300

    os.walk tree

--- 2.shutill_os.py
+++ 2.shutill_os.py
@@ -20,7 +20,9 @@ print(os.listdir('C:\\Users')) # All items in this directory
 
 import shutil
 
-shutil.move(src='practice.txt',dst='C:\\Users\kingt\OneDrive\Desktop')
+n2 = int(input('_'))
+if n2 == 2:
+    shutil.move(src='practice.txt',dst='C:\\Users\kingt\OneDrive\Desktop')
 
 os.listdir('C:\\Users\kingt\OneDrive\Desktop')
 
@@ -35,3 +37,28 @@ NOTE: The os module provides 3 methods for deleting files:
 All of these methods can not be reversed! Which means if you make a mistake you won't be able to recover the file. 
 Instead we will use the send2trash module.
 A safer alternative that sends deleted files to the trash bin instead of permanent removal.*'''
+
+# pip install send2trash
+
+#################################################
+
+# send2trash.send2trash('practice.txt')
+
+#################################################
+
+os.getcwd()
+
+file_path = 'C:\\Users\\kingt\\OneDrive\\Desktop\\Zero2Hero\\Complete-Python-3-Bootcamp-master\\12-Advanced Python Modules\\Example_Top_Level'
+
+for folder, sub_folders, files in os.walk(file_path):
+
+    print(f'Currently looking at {folder}')
+    print('\n')
+    print('The subfolders are: ')
+    for sub_fold in sub_folders:
+        print(f'Subfolder: {sub_fold}')
+    print('\n')
+    print('the files are: ')
+    for f in files:
+        print(f'\t File: {f}')
+    print('\n')
